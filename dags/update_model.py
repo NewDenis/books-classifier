4345999282cf41419a3ced48cf4feca50ca82996
NewDenis <pimenov.d.v@yandex.ru>
2023-08-13 17:05:06 +0000

    dag for prepare, train and save

--- dags/update_model.py
+++ dags/update_model.py
@@ -10,6 +10,20 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
 # Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
+from kubernetes.client import models as k8s
+
+volume_mount = k8s.V1VolumeMount(
+    name="books-volume",
+    mount_path="/mnt/data",
+    sub_path=None,
+)
+
+volume = k8s.V1Volume(
+    name="books-volume",
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(
+        claim_name="books-volume"
+    ),
+)
 
 
 # These args will get passed on to each operator
@@ -25,6 +39,20 @@ with DAG(
         task_id="prepare_data",
         image="pimenovdv/books-classifier:0.2.0",
         cmds=["python"],
-        arguments=["./scripts/prepare_data.py", "--help"],
+        arguments=["./scripts/prepare_data.py"],
         image_pull_policy="Always",
+        volume_mounts=[volume_mount],
+        volumes=[volume],
     )
+
+    t2 = KubernetesPodOperator(
+        task_id="train_save",
+        image="pimenovdv/books-classifier:0.2.0",
+        cmds=["python"],
+        arguments=["./scripts/train_and_save.py"],
+        image_pull_policy="Always",
+        volume_mounts=[volume_mount],
+        volumes=[volume],
+    )
+
+    t1 >> t2
